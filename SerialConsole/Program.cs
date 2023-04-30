34af5b8b139af44653e10ac4b9d9b83f2e61cacf
Lasha Morgoshia <xnagamer@live.com>
2023-04-30 10:47:05 +0400

    Update

---
+++ SerialConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialConsole
+{
+    internal class Program
+    {
+        static SerialPort serialPort;
+        static int Id = 6;
+
+        /// <summary>
+        /// სერიალის ინიცირება
+        /// </summary>
+        static void Config()
+        {
+            Console.Write("Please enter a port number to listen:");
+            Id = int.Parse(Console.ReadLine());
+            serialPort = new SerialPort($"COM{Id}", 9600); // Replace 9600 with the baud rate of your device
+            EnsurePortIsOpen();
+        }
+
+        /// <summary>
+        /// პორტის გახსნა
+        /// </summary>
+        static void EnsurePortIsOpen()
+        {
+            while (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Waiting for COM{Id} to open ..");
+                    Thread.Sleep(5000);
+                }
+            }
+
+            Console.WriteLine($"COM{Id} is Opened, listening to bytes ..");
+        }
+
+        static void Read()
+        {
+            Config();
+
+            try
+            {
+                string data = "";
+                while (true && serialPort.IsOpen)
+                {
+                    if (serialPort.BytesToRead > 0)
+                    {
+                        byte[] buffer = new byte[serialPort.BytesToRead];
+                        serialPort.Read(buffer, 0, buffer.Length);
+                        data += System.Text.Encoding.ASCII.GetString(buffer);
+                        if (data.IndexOf("|") > 0)
+                        {
+                            var lines = data.Split('|');
+                            foreach (var line in lines)
+                            {
+                                if (!string.IsNullOrEmpty(line))
+                                    Console.WriteLine(line);
+                            }
+                            data = "";
+                        }
+                    }
+                }
+            }
+            catch { }
+
+            Read();
+        }
+
+        static void Main(string[] args)
+        {
+            Read();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SerialListener/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialListener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            this.textBox1.Text += e.ToString();
+        }
+
+        private void serialPort1_ErrorReceived(object sender, System.IO.Ports.SerialErrorReceivedEventArgs e)
+        {
+            this.textBox1.Text += e.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.serialPort1.Open();
+            
+        }
+    }
+}
